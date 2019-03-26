0aaa315d31d664162d483c78c72f273e607f1044
Matthew Fitzgerald <mattfitz0421@icloud.com>
2019-03-26 16:55:28 -0400

    Initial commit

---
+++ main.js
@@ -0,0 +1,37 @@
+//http://api.icndb.com/jokes/random
+
+let url = "https://api.icndb.com/jokes/random";
+
+function getJokes() {
+  confirm("\"You are willingy exposing yourself to possible attack and injury by reading about such an entity, do you understand?\"")
+  fetch(url)
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error(response.statusText);
+    })
+
+    .then(responseJson => displayResults(responseJson))
+    .catch(err => {
+      alert("Something went wrong: " + err);
+    })
+    
+}
+
+function displayResults(inval){
+
+  $("#replaceMe").html(inval.value.joke);
+}
+
+function openFacebook() {
+window.location.href = "https://www.facebook.com/officialchucknorrispage/";
+}
+
+function openReddit() {
+  window.location.href = "https://www.reddit.com/r/ChuckNorris/";
+}
+
+function openTwitter() {
+  window.location.href = "https://twitter.com/chucknorris?lang=en";
+}
